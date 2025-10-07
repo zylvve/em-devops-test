7bfba9f8b608d7a3240ad91e91f5bd036106b9a9
Vadim <zylvve@proton.me>
2025-10-07 19:24:34 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,18 +16,18 @@ while true; do
 
     if [ -n "$new_pid" ]; then
 	# Проверяю на перезапуск через id процесса
-	if [ "$new_pid" != "$initial_pid" ]; then
-     	    echo "$(date): Процесс $PROCESS_NAME был перезапущен" >> "$LOGFILE"
-            initial_pid=$new_pid
-	fi
-
-	# Код состояния ответа
-	status_code=$(curl -s -o /dev/null "$SERVER_URL" -w "%{http_code}" )
-
-	# Если совсем недостучались до сервера и не получили даже код ошибки 
-	if [ "$status_code" -eq 0 ]; then
-	    echo "$(date): Сервер не доступен" >> "$LOGFILE"  
-	fi
+		if [ "$new_pid" != "$initial_pid" ]; then
+	     	    echo "$(date): Процесс $PROCESS_NAME был перезапущен" >> "$LOGFILE"
+	            initial_pid=$new_pid
+		fi
+	
+		# Код состояния ответа
+		status_code=$(curl -s -o /dev/null "$SERVER_URL" -w "%{http_code}" )
+	
+		# Если совсем недостучались до сервера и не получили даже код ошибки 
+		if [ "$status_code" -eq 0 ]; then
+		    echo "$(date): Сервер не доступен" >> "$LOGFILE"  
+		fi
     fi
 
     sleep 60;
